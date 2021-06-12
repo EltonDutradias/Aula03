9b842baccad8d43c84c6795e98b1b928ac759380
Elton Dutra Dias <elton.dutradias.ti@gmail.com>
2021-06-12 13:51:24 -0300

    Desenvolvendo Exercicio Triângulo

--- src/ExercicioTriangulo.java
+++ src/ExercicioTriangulo.java
@@ -1,9 +1,23 @@
+import java.util.Scanner; 
 
 public class ExercicioTriangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner leitor = new Scanner(System.in); 
+	double ladoa, ladob, ladoc; 
+	System.out.println("Digite a medida do lado a da figura"); 
+	ladoa = leitor.nextDouble();
+	System.out.println("Digite a medida do lado b da figura"); 
+	ladob = leitor.nextDouble();
+	System.out.println("Digite a medida do lado c da figura"); 
+	ladoc = leitor.nextDouble();
+      
+	if ((ladoa + ladob > ladoc) && (ladob + ladoc > ladoa) && (ladoa + ladoc > ladob)) {
+		System.out.println("O triângulo existe");
+		}else {
+		System.out.println("O triângulo não existe");
+		}
+	   leitor.close();
 	}
-
+     
 }
