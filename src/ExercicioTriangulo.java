36e54a3b5066a19ba5c4d53e3c3b849f000a554d
Elton Dutra Dias <elton.dutradias.ti@gmail.com>
2021-06-13 09:12:12 -0300

    Correção de erro de comparação de valores e exclusão do Drone2

--- src/ExercicioTriangulo.java
+++ src/ExercicioTriangulo.java
@@ -10,13 +10,21 @@ public class ExercicioTriangulo {
 	System.out.println("Digite a medida do lado b da figura"); 
 	ladob = leitor.nextDouble();
 	System.out.println("Digite a medida do lado c da figura"); 
-	ladoc = leitor.nextDouble();
+	ladoc = leitor.nextDouble(); 
+	//2° etapa: validar seo tipo de triângulo existe
       
 	if ((ladoa + ladob > ladoc) && (ladob + ladoc > ladoa) && (ladoa + ladoc > ladob)) {
-		System.out.println("O triângulo existe");
+		System.out.println("O triângulo existe"); 
+		//3° etapa: validar tipo do triângulo 
+		if(ladoa == ladob && ladoa == ladoc) { 
+			System.out.println("O triângulo é equilatero");
+		}else if (ladoa != ladob && ladoa != ladoc && ladob != ladoc) { 
+			System.out.println("")
+		}
 		}else {
 		System.out.println("O triângulo não existe");
-		}
+		} 
+	
 	   leitor.close();
 	}
      
